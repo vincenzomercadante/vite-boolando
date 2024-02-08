2def5a922892ad81ec11ecbb5616337c4c4168e5
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-02-08 18:13:37 +0100

    feat: add main

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -48,7 +48,9 @@ export default {
 
 <template>
   <header>
+    <!-- container header links & logo -->
     <div class="container">
+      <!-- category links -->
       <ul>
         <li v-for="link in links">
           <a :href="link.href" :class="link.active ? 'active' : ''">{{
@@ -56,9 +58,11 @@ export default {
           }}</a>
         </li>
       </ul>
+      <!-- site logo -->
       <figure>
         <img :src="generateUrl(logo)" alt="boolando logo" />
       </figure>
+      <!-- header icon list -->
       <ul>
         <li v-for="icon in icons">
           <a :href="icon.href"><i :class="icon.img"></i></a>
@@ -86,7 +90,7 @@ header {
     font-weight: bold;
   }
   figure {
-    width: 200px;
+    width: $logo-width;
   }
 }
 </style>
---
+++ src/components/CardHearth.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  data() {
+    return {
+      isClicked: false,
+    };
+  },
+
+  methods: {
+    isClicked() {
+      this.isClicked = this.isClicked ? false : true;
+    },
+  },
+};
+</script>
+
+<template>
+  <div @click="isClicked()" :class="isClicked ? 'clicked' : ''">
+    <i class="fa-solid fa-heart"></i>
+  </div>
+</template>
+
+<style scoped lang="scss">
+div {
+  position: absolute;
+  right: 0;
+  top: 20px;
+  height: 50px;
+  aspect-ratio: 1;
+  line-height: 50px;
+  font-size: 1.2rem;
+  text-align: center;
+  background-color: white;
+
+  i {
+    color: black;
+  }
+}
+
+.clicked i {
+  color: red;
+}
+</style>
