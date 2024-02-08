4bad7942c2577e201459769e0654423607cd1f58
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-02-08 17:10:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+};
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+  <AppFooter></AppFooter>
+</template>
+
+<style lang="scss">
+@use "./assets/styles/general.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,92 @@
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          text: "Donna",
+          href: "#",
+          active: false,
+        },
+        {
+          text: "Uomo",
+          href: "#",
+          active: false,
+        },
+        {
+          text: "Bambino",
+          href: "#",
+          active: false,
+        },
+      ],
+      icons: [
+        {
+          img: "fa-regular fa-user",
+          href: "#",
+        },
+        {
+          img: "fa-regular fa-heart",
+          href: "#",
+        },
+        {
+          img: "fa-solid fa-bag-shopping",
+          href: "#",
+        },
+      ],
+
+      logo: "../assets/img/boolean-logo.png",
+    };
+  },
+
+  methods: {
+    generateUrl(path) {
+      return new URL(path, import.meta.url).href;
+    },
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <ul>
+        <li v-for="link in links">
+          <a :href="link.href" :class="link.active ? 'active' : ''">{{
+            link.text
+          }}</a>
+        </li>
+      </ul>
+      <figure>
+        <img :src="generateUrl(logo)" alt="boolando logo" />
+      </figure>
+      <ul>
+        <li v-for="icon in icons">
+          <a :href="icon.href"><i :class="icon.img"></i></a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/styles/partials/variables" as *;
+header {
+  display: flex;
+  justify-content: center;
+  background-color: $header-background;
+  box-shadow: 0px 3px 15px 1px rgba(0, 0, 0, 0.41);
+
+  .container {
+    padding: $header-padding;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: white;
+    font-size: 1rem;
+    font-weight: bold;
+  }
+  figure {
+    width: 200px;
+  }
+}
+</style>
